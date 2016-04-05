64c919c29a42e4465912a077083fa12c9e649803
NMVW <nmvinson@gmail.com>
2016-04-04 21:26:52 -0400

    Wired up Main Controller to output factory Storage data to view * need to create a factory/service for grabbing weather data from API endpoints

--- public/app.js
+++ public/app.js
@@ -1,7 +1,22 @@
 angular.module('feelslike', [])
 
-.controller()
-
-.factory()
-
+.controller('MainController', function($scope, Storage) {
+  // $scope.names = [Robin, Bryan, Nick];
+  console.log(Storage.names);
+  $scope.names = Storage.names;
+  $scope.print = Storage.print;
+  console.log('Main controller is running');
+})
+.factory('Storage', function() {
+  var cities = ['San Francisco','New York', 'St. Paul'];
+  
+  var print = function(place) {
+    console.log(place);
+  };
+  
+  return {
+    print: print,
+    names: cities
+  };
+})
 .run();
