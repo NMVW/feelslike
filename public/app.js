2afff1cc8a7f0260e311e0d70e74fc5ddc9b9aad
NMVW <nmvinson@gmail.com>
2016-04-04 23:26:56 -0400

    API endpoints returning data successfully.
    * next step is to integrate into view

--- public/app.js
+++ public/app.js
@@ -1,3 +1,7 @@
+// var API_KEY = require('./key.js');
+var API_KEY = '';
+console.log(API_KEY);
+
 angular.module('feelslike', [])
 
 .controller('MainController', function($scope, Storage) {
@@ -5,22 +9,39 @@ angular.module('feelslike', [])
   console.log(Storage.names);
   $scope.names = Storage.names;
   $scope.print = Storage.print;
+  $scope.checkWeather = Storage.checkWeather;
+  
   console.log('Main controller is running');
 })
-.factory('Storage', function() {
+.factory('Storage', function($http) {
   var cities = ['San Francisco','New York', 'St. Paul'];
   
   var print = function(place) {
     console.log(place);
   };
   
+  var checkWeather = function(place) {
+    
+    place = place.replace(/ /g, '+');
+    
+    $http({
+      method: 'GET',
+      url: 'http://api.openweathermap.org/data/2.5/weather?q='+place+'&APPID=' + API_KEY
+    }).then(function(res) {
+      console.log(res.data);
+    }, function(err) {
+      console.log(err);
+    });
+  };
   
+  // console.log(API_KEY);
   // getting LIVE weather data from OpenWeatherMap API
   // http://openweathermap.org/current
   
   return {
     print: print,
-    names: cities
+    names: cities,
+    checkWeather: checkWeather
   };
 })
 .run();
