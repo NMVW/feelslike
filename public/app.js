dd9ddc0617f0561a4c215144970dc449d99c0985
NMVW <nmvinson@gmail.com>
2016-04-05 20:50:51 -0400

    API endpoints work locally

--- public/app.js
+++ public/app.js
@@ -70,7 +70,7 @@ angular.module('feelslike', [])
     function getWeather(latLon) {
       $http({
         method: 'GET',
-        url: 'https://api.openweathermap.org/data/2.5/find?lat='+latLon[0] +'&lon='+ latLon[1]+'&cnt='+cityCount+'&APPID=' + wAPI_KEY
+        url: 'http://api.openweathermap.org/data/2.5/find?lat='+latLon[0] +'&lon='+ latLon[1]+'&cnt='+cityCount+'&APPID=' + wAPI_KEY
       }).then(function(res) {
         cb(res.data);
       }, function(err) {
@@ -84,7 +84,7 @@ angular.module('feelslike', [])
   function getCoordinates(city, cb) {
     $http({
       method: 'GET',
-      url: 'https://api.opencagedata.com/geocode/v1/json?q='+city+'&key='+gAPI_KEY
+      url: 'http://api.opencagedata.com/geocode/v1/json?q='+city+'&key='+gAPI_KEY
     }).then(function(res) {
       var lat = res.data.results[0].bounds.northeast.lat;
       var lon = res.data.results[0].bounds.northeast.lng;
@@ -99,7 +99,7 @@ angular.module('feelslike', [])
     cit = city.replace(/ /g, '+');
     $http({
         method: 'GET',
-        url: 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=' + wAPI_KEY
+        url: 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=' + wAPI_KEY
       }).then(function(res) {
         console.log('weather data:');
         console.log(res.data);
@@ -132,7 +132,7 @@ angular.module('feelslike', [])
     function getCoordinates(city, cb) {
       $http({
         method: 'GET',
-        url: 'https://api.opencagedata.com/geocode/v1/json?q='+city+'&key='+gAPI_KEY
+        url: 'http://api.opencagedata.com/geocode/v1/json?q='+city+'&key='+gAPI_KEY
       }).then(function(res) {
         // console.log(res);
         cb(res);
@@ -144,7 +144,7 @@ angular.module('feelslike', [])
     function getRef(latLon, cb) {
       $http({
         method: 'GET',
-        url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+latLon[0]+','+latLon[1]+'&radius=1500&key='+placesAPI_KEY
+        url: 'http://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+latLon[0]+','+latLon[1]+'&radius=1500&key='+placesAPI_KEY
       }).then(function(data) {
         var reference = data.results[1].photos[0].photo_reference;
         cb(reference);
@@ -154,7 +154,7 @@ angular.module('feelslike', [])
     function getPic(ref, cb) {
       $http({
         method: 'GET',
-        url: 'https://maps.googleapis.com/maps/api/place/photo?photo_reference'+ref+'&maxheight=100&key='+placesAPI_KEY
+        url: 'http://maps.googleapis.com/maps/api/place/photo?photo_reference'+ref+'&maxheight=100&key='+placesAPI_KEY
       }).then(function(html) {
         //extract img from url and send in cb
         console.log('here is random pic object:');
